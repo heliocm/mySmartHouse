fff5a6e2df9b30a98ecc3ee54cb930d0d9d46011
Helio Cheble Machado <heliocm@poli.ufrj.br>
2015-07-21 03:01:57 -0300

    Prova de conceito do projeto para demonstração da disciplina Software para Smartphones

---
+++ Arduino/lightsOnOutput13/lightsOnOutput13.ino
@@ -0,0 +1,23 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+void loop()
+{
+  if (Serial.available())
+  {
+    char ch = Serial.read();
+    if (ch == '0')
+    {
+      digitalWrite(13, LOW);
+      Serial.println("Desliga lampada");
+      Serial.println(digitalRead(13));
+    }
+    if (ch == '1')
+    {
+      digitalWrite(13, HIGH);
+      Serial.println("Liga lampada");
+      Serial.println(digitalRead(13));
+    }
+  }
+}
